0a065bebd3230e97559e5066fd049e415faa1cde
Wallace Benevides <wallacebenevides@hotmail.com>
2019-12-02 19:16:06 -0300

    Using Router

--- src/Header.js
+++ src/Header.js
@@ -1,17 +1,19 @@
 import React from 'react';
+import LinkWrapper from './LinkWrapper';
 
-const Header = () =>{
+const Header = () => {
     return (
         <nav>
-                <div className="nav-wrapper indigo lighten-2">
-                    <a href="/" className="brand-logo ml-3">Casa do Código</a>
-                    <ul id="nav-mobile" className="right">
-                        <li><a href="/autores">Autores</a></li>
-                        <li><a href="/livros">Livros</a></li>
-                        <li><a href="/sobre">Sobre</a></li>
-                    </ul>
-                </div>
-            </nav>
+        <div className="nav-wrapper indigo lighten-2">
+          <LinkWrapper to="/" className="brand-logo">Casa do Código</LinkWrapper>
+          <ul className="right">
+            <li><LinkWrapper to='/autores'>Autores</LinkWrapper></li>
+            <li><LinkWrapper to='/livros'>Livros</LinkWrapper></li>
+            <li><LinkWrapper to='/sobre'>Sobre</LinkWrapper></li>
+          </ul>
+        </div>
+      </nav>
     );
 }
+
 export default Header;
--- src/index.js
+++ src/index.js
@@ -3,8 +3,24 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Livros from './Livros';
+import Sobre from './Sobre';
+import Autores from './Autores';
+import NotFound from './NotFound';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <BrowserRouter>
+        <Switch>
+            <Route path="/" exact={true} component={App} />
+            <Route path="/sobre" component={Sobre} />
+            <Route path="/livros" component={Livros} />
+            <Route path="/autores" component={Autores} />
+            <Route component={NotFound} />
+        </Switch>
+    </BrowserRouter>,
+    document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
